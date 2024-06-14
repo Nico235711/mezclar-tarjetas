6eec6ac9dc0cc5f19ef04b2be07b778adfd46361
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-06-14 15:50:07 -0300

    mezclando cartas

--- js/javascript.js
+++ js/javascript.js
@@ -1,30 +1,39 @@
-var grupoTarjetas = ["🦄", "🍦", "🌈", "👽", "👾", "🤖", "👹", "👺"];
+const grupoTarjetas = ["🦄", "🍦", "🌈", "👽", "👾", "🤖", "👹", "👺"];
 
 // EJERCICIO: concatena grupoTarjetas consigo misma para duplicar
 // los valores
-var totalTarjetas = grupoTarjetas;
+const totalTarjetas = grupoTarjetas.concat(grupoTarjetas);
 
 function barajaTarjetas() {
-  var resultado;
-  resultado = totalTarjetas.sort(function() {
-    return 0.5 - Math.random();
-  });
+  let resultado;
+  resultado = totalTarjetas.sort(() => 0.5 - Math.random());
   return resultado;
 }
 
 function reparteTarjetas() {
-  var mesa = document.querySelector("#mesa");
-  var tarjetasBarajadas = barajaTarjetas();
+  const mesa = document.querySelector("#mesa");
+  const tarjetasBarajadas = barajaTarjetas();
   mesa.innerHTML = "";
 
   // EJERCICIO: recorre el array de tarjetasBarajadas y para cada
-  // elemento, crea un div (cárgalo en una variable) y declara que su
+  // elemento, crea un div (cárgalo en una constiable) y declara que su
   // innerHTML es el div con la clase "tarjeta" y en el interior
   // del mismo, un div con la clase "tarjeta__contenido".
   // Añade además el valor del elemento como contenido de la tarjeta
-
-  // EJERCICIO: una vez has creado la tarjeta, y todavía dentro del
-  // loop, añade la tarjeta generada como "child" del nodo mesa.
+  tarjetasBarajadas.forEach(elemento => {
+    let divTarjeta = document.createElement("div")
+    divTarjeta.innerHTML = `<div class="tarjeta">
+      <div class="tarjeta__contenido">
+        ${elemento}
+      </div>
+    </div>
+    `
+    // console.log(divTarjetas);
+    // EJERCICIO: una vez has creado la tarjeta, y todavía dentro del
+    // loop, añade la tarjeta generada como "child" del nodo mesa.
+    mesa.append(divTarjeta)
+  })
+  
 
   // BONUS: podrías hacer que las tarjetas solo se repartieran cuando
   // se apriete un botón que ponga "repartir cartas"
